d3e09586eac18200de67119a234827d367cc9c3e
Nykolas Perazzo <nykolasrg@icloud.com>
2025-08-02 08:29:59 -0300

    atualização v1.7

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,15 @@
-// This file is intentionally left blank.
+const menuToggle = document.getElementById('menuToggle');
+const menuList = document.getElementById('menuList');
+const icon = menuToggle.querySelector('i');
+
+menuToggle.addEventListener('click', () => {
+    menuList.classList.toggle('active');
+
+    if (menuList.classList.contains('active')) {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-x');
+    } else {
+        icon.classList.remove('fa-x');
+        icon.classList.add('fa-bars');
+    }
+});
